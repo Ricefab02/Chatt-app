85fc89c700069a3258c269e4a32526faceca1afd
Ricefab02 <roger.fabrice02@gmail.com>
2020-04-13 19:47:49 +0200

    add onClick and state in contact js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,33 +5,49 @@ import PropTypes from "prop-types";
 class Contact extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {online : props.online};
-    render() {
-      return (<div className="Contact">
+    this.state = {
+      online: props.online
+    };
+    
+  }
+
+  render() {
+    return (
+      <div className="Contact">
         <img className="avatar" src={this.props.avatar} alt={this.props.name} />
         <div>
-          <p className="name">{props.name}</p>
-          <div className="status">
-            <div className={this.state.online ? "status-online" : "status-offline"} />
-            <p className="status-text">
-              {this.state.online ? "status-online" : "status-offline"}
+          <p className="name">{this.props.name}</p>
+          <div className="status"
+          onClick={event => {
+            const newOnline = !this.state.online;
+            this.setState({ online: newOnline });
+          }}
+          
+          >
+           
+            <div
+              className={this.state.online ? "status-online" : "status-offline"}
               onClick={event => {
                 const newOnline = !this.state.online;
                 this.setState({ online: newOnline });
               }}
+            />
+            <p className="status-text">
+              {this.state.online ? "online" : "offline"}
+              
             </p>
           </div>
         </div>
-      </div>);
-    };
-  };
-};
+      </div>
+    );
+  }
+}
+
 Contact.propTypes = {
   name: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
   online: PropTypes.bool.isRequired
 };
 export default Contact;
-
 
 
